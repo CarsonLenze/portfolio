91b6b3f02ed321a1a629e487ed89fb3a4eb1932f
CarsonLenze <carsonlenze@gmail.com>
2022-06-28 21:32:31 -0400

    Commit

--- pages/api/interactions.js
+++ pages/api/interactions.js
@@ -14,9 +14,9 @@ export default async function interactions(req, res) {
 
     try {
         isVerified = nacl.sign.detached.verify(
-            Buffer.from(req.headers['X-Signature-Timestamp'] + buf),
-            Buffer.from(req.headers['X-Signature-Ed25519'], 'hex'),
-            Buffer.from(process.env.PUBLIC_KEY, 'hex')
+            Buffer.from(req.headers['x-signature-timestamp'] + buf),
+            Buffer.from(req.headers['x-signature-ed25519'], 'hex'),
+            Buffer.from('47b20d6ba6bff9c2187dd6d5ff9ce03b13231f93287eb1f05304c6eca37a6c28', 'hex')
         );
     } catch {
         return res.status(401).end('invalid request signature');
@@ -26,24 +26,22 @@ export default async function interactions(req, res) {
         return res.status(401).end('invalid request signature');
     }
 
-    // let json = buf.toString()
-    // json = JSON.parse(json)
+    let json = buf.toString()
+    json = JSON.parse(json)
 
-    // if (json.type == 1) {
-    //     return res.send({
-    //         "type": 1,
-    //     })
-    // }
+    if (json.type == 1) {
+        return res.send({
+            "type": 1,
+        })
+    }
 
-    // const { attachments } = json.data.resolved;
-    // const attachment = Object.entries(attachments)[0];
+    const { attachments } = json.data.resolved;
+    const attachment = Object.entries(attachments)[0];
 
-    // res.send({
-    //     "type": 4,
-    //     "data": {
-    //         "content": attachment[1].url
-    //     }
-    // })
-
-
+    res.send({
+        "type": 4,
+        "data": {
+            "content": attachment[1].url
+        }
+    })
 }
