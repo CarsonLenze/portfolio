a1c39788df7a35da33ffcb7d969b59ebcc3e9d56
CarsonLenze <carsonlenze@gmail.com>
2022-06-28 20:45:11 -0400

    Commit

--- pages/api/interactions.js
+++ pages/api/interactions.js
@@ -1,8 +1,8 @@
 export const config = {
     api: {
-      bodyParser: false,
+        bodyParser: false,
     },
-  };
+};
 
 const { buffer } = require('micro');
 const nacl = require('tweetnacl');
@@ -13,30 +13,37 @@ export default async function interactions(req, res) {
     let isVerified;
 
     try {
-    isVerified = nacl.sign.detached.verify(
-      Buffer.from(req.headers['X-Signature-Timestamp'] + buf),
-      Buffer.from(req.headers['X-Signature-Ed25519'], 'hex'),
-      Buffer.from(process.env.PUBLIC_KEY, 'hex')
-    );
+        isVerified = nacl.sign.detached.verify(
+            Buffer.from(req.headers['X-Signature-Timestamp'] + buf),
+            Buffer.from(req.headers['X-Signature-Ed25519'], 'hex'),
+            Buffer.from(process.env.PUBLIC_KEY, 'hex')
+        );
     } catch {
         return res.status(401).end('invalid request signature');
     }
-    
+
     if (!isVerified) {
-      return res.status(401).end('invalid request signature');
+        return res.status(401).end('invalid request signature');
     }
 
-    let json = buf.toString();
-    json = JSON.parse(json);
+    let json = buf.toString()
+    json = JSON.parse(json)
+
+    if (json.type == 1) {
+        return res.send({
+            "type": 1,
+        })
+    }
 
     const { attachments } = json.data.resolved;
     const attachment = Object.entries(attachments)[0];
 
-    res.send({ 
+    res.send({
         "type": 4,
         "data": {
             "content": attachment[1].url
-        } 
-      })
+        }
+    })
+
 
 }
