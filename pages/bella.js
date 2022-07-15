4cfeaa611ca63e1a33981b5c5dc33fbc7bcffed2
CarsonLenze <carsonlenze@gmail.com>
2022-07-15 04:02:49 -0400

    Update bella.js

--- pages/bella.js
+++ pages/bella.js
@@ -1,13 +1,26 @@
-export default function Home() {
+export default function Home(ip) {
 
     return (
         <div>
+            {ip}
             <img src="/bella/1.jpg" alt="Simply Easy Learning" ></img>
             <img src="/bella/2.jpg" alt="Simply Easy Learning" ></img>
             <img src="/bella/3.jpg" alt="Simply Easy Learning" ></img>
             <img src="/bella/4.jpg" alt="Simply Easy Learning" ></img>
             <img src="/bella/5.jpg" alt="Simply Easy Learning" ></img>
-        
-            </div>
+        </div>
+
     )
 }
+
+export const getServerSideProps = async ({ req }) => {
+    const forwarded = req.headers['x-forwarded-for'];
+  
+    const ip = typeof forwarded === 'string' ? forwarded.split(/, /)[0] : req.socket.remoteAddress;
+  
+    console.log(forwarded);
+  
+    return {
+      props: { ip },
+    };
+  };
