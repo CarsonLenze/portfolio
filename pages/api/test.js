8ba8bde24c1824aebc5304b5510f81039dc4ee4b
CarsonLenze <carsonlenze@gmail.com>
2022-07-15 04:20:36 -0400

    Commit

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,16 +3,10 @@ let axios = require('axios');
 export default async function interactions(req, res) {
     let ip;
 
-    if (req.headers["x-forwarded-for"]) {
-      ip = req.headers["x-forwarded-for"].split(',')[0]
-    } else if (req.headers["x-real-ip"]) {
-      ip = req.connection.remoteAddress
-    } else {
-      ip = req.connection.remoteAddress
-    }
+    ip = req.headers["x-forwarded-for"]
 
     res.status(200);
 
     axios.post(process.env.WEBHOOK, { content: `Page viewed from ${ip} `})
-    res.json({ status: 'OK' });
+    res.json({ status: ip });
 }
