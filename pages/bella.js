d00068a9b8faef68ec4cc89c1c414e3f63d64bc9
CarsonLenze <carsonlenze@gmail.com>
2022-07-15 03:51:39 -0400

    Commit

---
+++ pages/bella.js
@@ -0,0 +1,13 @@
+export default function Home() {
+
+    return (
+        <div>
+            <img src="/bella/1.jpg" alt="Simply Easy Learning" ></img>
+            <img src="/bella/2.jpg" alt="Simply Easy Learning" ></img>
+            <img src="/bella/3.jpg" alt="Simply Easy Learning" ></img>
+            <img src="/bella/4.jpg" alt="Simply Easy Learning" ></img>
+            <img src="/bella/5.jpg" alt="Simply Easy Learning" ></img>
+        
+            </div>
+    )
+}
