0bff789c99eeccbf51668fbef9a5517459e8b8ef
CarsonLenze <carson@overfuel.com>
2024-08-13 13:21:05 -0400

    progress

--- src/components/modules/Hero.js
+++ src/components/modules/Hero.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 export default function Hero(props) {
 
     return (
-        <Container id="home" fluid className="d-flex align-items-center justify-content-center vh-100 primary-background">
+        <Container id="home" fluid className="d-flex align-items-center justify-content-center vh-100 primary-background hero-offset">
             <div>
                 <h1>Hero area</h1>
                 <p>This container takes up the full height of the screen.</p>
--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,28 +1,91 @@
 import { Navbar, Nav, Container, Offcanvas } from 'react-bootstrap';
+import { Link } from 'react-scroll';
 
 export default function Header() {
 
+    const duration = 150;
+
     return (
-        <Navbar sticky="top" className='custom-header' expand="lg">
+        <Navbar sticky="top" className='custom-header' expand="lg" variant="dark">
             <Container fluid>
-                <Navbar.Brand href="#">Navbar</Navbar.Brand>
+            <Navbar.Brand className='nav-brand'>
+            <img
+              alt="B"
+              src="https://react-bootstrap.netlify.app/img/logo.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{''}
+            ootstrap
+          </Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Offcanvas
                     placement="end"
+                    data-bs-theme="dark"
+                    className="secondary-background custom-offcanvas"
                 >
                     <Offcanvas.Header closeButton>
-                        <Offcanvas.Title >
-                            Offcanvas
+                        <Offcanvas.Title as="h1">
+                            Quick Links
                         </Offcanvas.Title>
                     </Offcanvas.Header>
 
                     <Offcanvas.Body>
                         <Nav className="justify-content-end flex-grow-1 pe-3">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="#skills">Skills</Nav.Link>
-                            <Nav.Link href="#projects">Projects</Nav.Link>
-                            <Nav.Link href="#contact">Contact</Nav.Link>
+                            <Link
+                                to="home"
+                                smooth={true}
+                                duration={duration}
+                                spy={true}
+                                activeClass="active"
+                                className="nav-link"
+                            >
+                                Home
+                            </Link>
+
+                            <Link
+                                to="about"
+                                smooth={true}
+                                duration={duration}
+                                spy={true}
+                                activeClass="active"
+                                className="nav-link"
+                            >
+                                About
+                            </Link>
+
+                            <Link
+                                to="skills"
+                                smooth={true}
+                                duration={duration}
+                                spy={true}
+                                activeClass="active"
+                                className="nav-link"
+                            >
+                                Skills
+                            </Link>
+
+                            <Link
+                                to="projects"
+                                smooth={true}
+                                duration={duration}
+                                spy={true}
+                                activeClass="active"
+                                className="nav-link"
+                            >
+                                Projects
+                            </Link>
+
+                            <Link
+                                to="contact"
+                                smooth={true}
+                                duration={duration}
+                                spy={true}
+                                activeClass="active"
+                                className="nav-link"
+                            >
+                                Contact
+                            </Link>
                         </Nav>
                     </Offcanvas.Body>
 
